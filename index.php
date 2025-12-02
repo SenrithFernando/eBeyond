e9663e2bd976d39e1ae94402cdeb876589d90a64
Senrith Fernando <senrithfernando@Senriths-MacBook-Air.local>
2025-12-02 18:40:36 +0530

    footer section

--- index.php
+++ index.php
@@ -7,12 +7,16 @@
 
     <!-- bootstrap -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <!-- Bootstrap Icons -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.1/font/bootstrap-icons.css">
+
     
     <!-- Custom CSS -->
     <link rel="stylesheet" href="./css/nav.css">
     <link rel="stylesheet" href="./css/hero.css">
     <link rel="stylesheet" href="./css/fav.css">
     <link rel="stylesheet" href="./css/contact.css">
+    <link rel="stylesheet" href="./css/fot.css">
 
 </head>
 <body>
@@ -24,6 +28,8 @@
     <?php include './library/home/favsection.php'; ?>
     <!-- Contact Us Section -->
     <?php include './library/home/contactus.php'; ?>
+    <!-- Footer -->
+    <?php include './library/head/footer.php'; ?>
 
 
    
---
+++ library/head/footer.php
@@ -0,0 +1,43 @@
+<footer class="site-footer">
+  <div class="container footer-inner">
+
+    <!-- TOP ROW -->
+    <div class="row footer-top align-items-start">
+
+      <!-- LEFT: Address -->
+      <div class="col-lg-6 col-md-6 col-12 footer-left mb-3 mb-md-0">
+        <h4 class="footer-title">IT Group</h4>
+        <p class="footer-text">C. Salvador de Madariaga, 1<br>
+          28027 Madrid<br>
+          Spain
+        </p>
+      </div>
+
+      <!-- RIGHT: Social -->
+      <div class="col-lg-6 col-md-6 col-12 footer-right text-md-end text-center">
+        <p class="footer-follow">Follow us on</p>
+        <div class="footer-icons">
+          <a href="#"><i class="bi bi-twitter"></i></a>
+          <a href="#"><i class="bi bi-youtube"></i></a>
+        </div>
+      </div>
+
+    </div>
+
+    <!-- DIVIDER LINE -->
+    <div class="footer-divider"></div>
+
+    <!-- BOTTOM ROW -->
+    <div class="footer-bottom d-flex flex-column flex-md-row
+                justify-content-between align-items-center">
+    <p class="footer-copy mb-1 mb-md-0">
+        Copyright © 2025 Senrith Fernando. All rights reserved.
+    </p>
+    <p class="footer-credit mb-0 text-md-end">
+        Photo by Freepic &amp; Vector and Image.
+    </p>
+    </div>
+
+
+  </div>
+</footer>
